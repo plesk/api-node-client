93fc2ca6eb13ef90a907c2a4ba5dce248fc710fd
Alexei Yuzhakov <sibprogrammer@gmail.com>
2017-01-24 22:40:26 +0700

    Use more common indentation for JS.

--- index.js
+++ index.js
@@ -3,53 +3,53 @@ const https = require('https');
 
 class Client {
 
-    constructor(host, port, protocol) {
-        this._host = host;
-        this._port = port || 8443;
-        this._protocol = protocol || 'https';
+  constructor(host, port, protocol) {
+    this._host = host;
+    this._port = port || 8443;
+    this._protocol = protocol || 'https';
+  }
+
+  setCredentials(login, password) {
+    this._login = login;
+    this._password = password;
+  }
+
+  setSecretKey(secretKey) {
+    this._secretKey = secretKey;
+  }
+
+  request(body, callback) {
+    let headers = {
+      'Content-type': 'text/xml',
+      'HTTP_PRETTY_PRINT': 'TRUE'
+    };
+
+    if (this._secretKey) {
+      headers['KEY'] = this._secretKey;
+    } else {
+      headers['HTTP_AUTH_LOGIN'] = this._login;
+      headers['HTTP_AUTH_PASSWD'] = this._password;
     }
 
-    setCredentials(login, password) {
-        this._login = login;
-        this._password = password;
-    }
+    let options = {
+      host: this._host,
+      port: this._port,
+      path: '/enterprise/control/agent.php',
+      method: 'POST',
+      headers: headers
+    };
 
-    setSecretKey(secretKey) {
-        this._secretKey = secretKey;
-    }
+    let handler = (response) => {
+      let result = '';
+      response.on('data', (chunk) => result += chunk);
+      response.on('end', () => callback(result));
+    };
 
-    request(body, callback) {
-        let headers = {
-            'Content-type': 'text/xml',
-            'HTTP_PRETTY_PRINT': 'TRUE'
-        };
-
-        if (this._secretKey) {
-            headers['KEY'] = this._secretKey;
-        } else {
-            headers['HTTP_AUTH_LOGIN'] = this._login;
-            headers['HTTP_AUTH_PASSWD'] = this._password;
-        }
-
-        let options = {
-            host: this._host,
-            port: this._port,
-            path: '/enterprise/control/agent.php',
-            method: 'POST',
-            headers: headers
-        };
-
-        let handler = (response) => {
-            let result = '';
-            response.on('data', (chunk) => result += chunk);
-            response.on('end', () => callback(result));
-        };
-
-        let client = 'https' == this._protocol ? https : http;
-        let request = client.request(options, handler);
-        request.write(body);
-        request.end();
-    }
+    let client = 'https' == this._protocol ? https : http;
+    let request = client.request(options, handler);
+    request.write(body);
+    request.end();
+  }
 
 }
 
